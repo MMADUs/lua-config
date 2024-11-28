266098df97e79f0314bef516687f43d9c554eccc
MMADUs <m.nizwa01@gmail.com>
2024-11-28 23:03:27 +0700

    inline hints

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,8 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader>ih", function()
+  vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled {})
+end)
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
